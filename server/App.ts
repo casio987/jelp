68a120afdd9c665cb37ee7e9ad242950bfdad6d6
sunny_thinkpad <sunnyw987@gmail.com>
2022-08-02 20:07:54 +1000

    implement server boilerplate

--- server/App.ts
+++ server/App.ts
@@ -1,4 +1,7 @@
 import express, { Application } from 'express';
+import cors from "cors";
+import mongoose from "mongoose";
+import { IController } from "./src/interfaces/IController";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -7,9 +10,34 @@ export class App {
   public express: Application;
   public port: number;
 
-  constructor(port: number) {
-    this.express = express();
+  constructor(controllers: IController[], port: number) {
+    this.express = this.setUpExpress();
     this.port = port;
+
+    this.initialiseDatabaseConnection();
+    this.initialiseControllers(controllers);
+  }
+
+  private setUpExpress = () => {
+    const app = express()
+      .use(cors())
+      .use(express.json())
+      .use(express.urlencoded({ extended: true }));
+    
+    return app;
+  }
+
+  private initialiseDatabaseConnection = () => {
+    // TODO; get mongodb uri
+    mongoose.connect("" || "asd", () => {
+      console.log('DB connected :)');
+    });
+  }
+
+  private initialiseControllers = (controllers: IController[]) => {
+    controllers.forEach((controller: IController) => {
+      this.express.use('/api', controller.getRouter());
+    })
   }
 
   public listen = () => {
--- server/server.ts
+++ server/server.ts
@@ -1,4 +1,5 @@
 import { App } from "./App";
 
-const app = new App(Number(process.env.PORT));
+// TODO: add controllers here
+const app = new App([], Number(process.env.PORT));
 app.listen();
