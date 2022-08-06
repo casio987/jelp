6a7042da9ce9387f445e197a4ee6b973370210c4
sunny_thinkpad <sunnyw987@gmail.com>
2022-08-06 11:09:10 +1000

    update header to include profile page path

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import SignUpPage from "./pages/SignUpPage/SignUpPage";
@@ -9,6 +10,7 @@ const Router = () => (
         <Route path='/' element={ <LandingPage /> } />
         <Route path='/signup' element={ <SignUpPage /> } />
         <Route path='/login' element={ <LoginPage /> } />
+        <Route path='/home' element={ <HomePage /> } />
       </Routes>
     </BrowserRouter>
 );
--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,14 +1,65 @@
 import { HeaderContainer, SignUpButton, Logo, LoginLink } from "./style";
 import { useNavigate } from "react-router-dom";
+import { Menu, IconButton, MenuItem } from "@mui/material";
+import { useState } from "react";
+import ProfileIcon from "../../assets/profile.png";
 
 const Header = () => {
   const navigate = useNavigate();
 
+  const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
+  const [menuOpen, setMenuOpen] = useState<boolean>(false);
+
+  const handleMenuOpen = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(e.currentTarget);
+    setMenuOpen(true);
+  }
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    setMenuOpen(false);
+  }
+
+  const handleLogout = () => {
+    // TODO: remove token
+    navigate('/', { replace: true });
+  }
+
   return (
     <HeaderContainer>
       <Logo onClick={() => navigate('/', { replace: true })}>jelp</Logo>
-      <LoginLink href="/login">Login</LoginLink>
-      <SignUpButton onClick={() => navigate('/signup', { replace: true })}>Sign up</SignUpButton>
+      {/* TODO replace with checking token */}
+      {false
+        ? 
+          (
+            <>
+              <IconButton
+                onClick={handleMenuOpen}
+                sx={{ marginLeft: "auto", height: "3rem" }}
+              >
+                <img src={ProfileIcon} style={{ height: '3rem' }} alt="profile" />
+              </IconButton>
+              <Menu
+                open={menuOpen}
+                anchorEl={anchorEl}
+                onClose={handleMenuClose}
+              >
+                <MenuItem onClick={() => navigate('/profile', { replace: true })}>
+                  Profile 
+                </MenuItem>
+                <MenuItem onClick={handleLogout}>
+                  Logout 
+                </MenuItem>
+              </Menu>
+            </>
+          )
+        : (
+          <>
+            <LoginLink href="/login">Login</LoginLink>
+            <SignUpButton onClick={() => navigate('/signup', { replace: true })}>Sign up</SignUpButton>
+          </>
+        )
+      }
     </HeaderContainer>
   )
 };
