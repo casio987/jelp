5c82e53120f486cfe9daa862906cd3199beb5477
sunny_thinkpad <sunnyw987@gmail.com>
2022-08-16 16:01:52 +1000

    feat(frontend): update interview review page with api call and happy/unhappy paths

--- client/src/pages/InterviewReviewPage/InterviewReviewPage.tsx
+++ client/src/pages/InterviewReviewPage/InterviewReviewPage.tsx
@@ -6,68 +6,119 @@ import { Rating } from "@mui/material";
 import ReviewPreviewCard from "../../components/ReviewPreviewCard/ReviewPreviewCard";
 import { useParams } from "react-router-dom";
 import { Palette } from "../../components/Palette";
+import { useEffect, useState } from "react";
+import { getInterviewReview } from "../../api/interview";
+import ErrorPopup from "../../components/Popup/Popup";
+import LoadingOverlay from "../../components/LoadingOverlay/LoadingOverlay";
 
 const InterviewReviewPage = () => {
   const { interviewReviewId } = useParams();
-  console.log(interviewReviewId);
-  // TODO: replace placholder text with fetch to grab data
+
+  const [errorMsg, setErrorMsg] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [jobTitle, setJobTitle] = useState<string>("Job Title");
+  const [companyName, setCompanyName] = useState<string>("Company name");
+  const [experience, setExperience] = useState<string>("Interview experience...");
+  const [questionsAsked, setQuestionsAsked] = useState<string>("Questions asked...");
+  const [rating, setRating] = useState<number>(0);
+  const [offerReceived, setOfferReceived] = useState<boolean>(false);
+  const [dateOfPost, setDateOfPost] = useState<string>("");
+
+  const loadInterviewReview = async () => {
+    try {
+      const { status, data } = await getInterviewReview(interviewReviewId!);
+      if (status === 200) {
+        setJobTitle(data.jobTitle);
+        setCompanyName(data.atCompany);
+        setExperience(data.experience);
+        setQuestionsAsked(data.questionsAsked);
+        setRating(data.rating);
+        setOfferReceived(data.offerReceived);
+        setDateOfPost(data.createdAt);
+      } else {
+        setErrorMsg(String(data));
+      }
+      setIsLoading(false);
+    } catch (err: any) {
+      setErrorMsg("A network error occurred. Please try again.");
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    loadInterviewReview();
+    // eslint-disable-next-line
+  }, [])
 
   return (
     <InterviewReviewPageContainer>
-      <Header />
-      <MainBodyContainer>
-        <TopContainer>
-          {/* TODO: current placeholder till i figure out how to store images in db */}
-          <img src={CompanyIcon} alt="companyLogo" />
-          <TopCenterContainer>
-            <h1>Frontend Engineer</h1>
-            <p>21st February 2022</p>
-            <Rating value={2} readOnly />
-          </TopCenterContainer>
-          <Tag backgroundColor={Palette.jelpGreen}>Offer received</Tag>
-        </TopContainer>
-        <h2>Application/Interview Experience</h2>
-        <BodyContainer>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc mattis ac odio non vulputate. Suspendisse et sem lorem. Sed lacinia vulputate consectetur. Donec ultrices fringilla massa eu commodo. Donec eu sollicitudin sem. Cras aliquam risus nec aliquet semper. Mauris vel varius nibh. Sed sed arcu condimentum, sodales quam a, faucibus urna. Aliquam ultrices ullamcorper lectus, id finibus lacus pretium eget.
-            Sed et ligula libero. Ut tincidunt odio quis sapien venenatis pulvinar. Vestibulum sapien sem, fringilla nec ipsum a, finibus accumsan orci. Nulla tincidunt lacus sed lacus condimentum vehicula. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent at dolor quis enim mattis sagittis nec eu neque. Phasellus et nunc nec leo interdum fringilla vel ut quam. Quisque elementum a arcu id maximus. Vivamus tristique, ligula in porta consectetur, metus neque scelerisque orci, eu venenatis odio turpis at felis.
-            Nullam maximus id tortor et aliquam. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer quis suscipit urna. Pellentesque vestibulum pellentesque dui, et convallis lorem lacinia non. Curabitur ullamcorper est sit amet pretium pharetra. Integer cursus massa dui, et fringilla quam maximus sed. Morbi congue porta eros, ut tincidunt diam efficitur a.
-            Nulla condimentum mi ut cursus maximus. Nunc vitae mi ullamcorper, dictum dui sit amet, consectetur enim. Mauris rhoncus vestibulum vestibulum. Aenean a mi non ex malesuada blandit id et massa. Nam vehicula massa vitae fringilla consectetur. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam consequat diam accumsan, placerat metus vel, gravida purus. Curabitur lobortis enim at dignissim sollicitudin. Aliquam augue quam, commodo a sem vel, venenatis sodales neque. Maecenas eu mollis sapien, vitae blandit quam. Quisque quis ultricies dolor. Praesent sodales urna nec ex accumsan gravida. Praesent quis tellus ut lectus mollis hendrerit. Maecenas finibus libero tellus. Ut semper posuere metus, at maximus orci facilisis in. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;         </p>
-        </BodyContainer>
-        <h2>Interview Questions</h2>
-        <BodyContainer>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc mattis ac odio non vulputate. Suspendisse et sem lorem. Sed lacinia vulputate consectetur. Donec ultrices fringilla massa eu commodo. Donec eu sollicitudin sem. Cras aliquam risus nec aliquet semper. Mauris vel varius nibh. Sed sed arcu condimentum, sodales quam a, faucibus urna. Aliquam ultrices ullamcorper lectus, id finibus lacus pretium eget.
-            Sed et ligula libero. Ut tincidunt odio quis sapien venenatis pulvinar. Vestibulum sapien sem, fringilla nec ipsum a, finibus accumsan orci. Nulla tincidunt lacus sed lacus condimentum vehicula. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent at dolor quis enim mattis sagittis nec eu neque. Phasellus et nunc nec leo interdum fringilla vel ut quam. Quisque elementum a arcu id maximus. Vivamus tristique, ligula in porta consectetur, metus neque scelerisque orci, eu venenatis odio turpis at felis.
-            Nullam maximus id tortor et aliquam. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer quis suscipit urna. Pellentesque vestibulum pellentesque dui, et convallis lorem lacinia non. Curabitur ullamcorper est sit amet pretium pharetra. Integer cursus massa dui, et fringilla quam maximus sed. Morbi congue porta eros, ut tincidunt diam efficitur a.
-            Nulla condimentum mi ut cursus maximus. Nunc vitae mi ullamcorper, dictum dui sit amet, consectetur enim. Mauris rhoncus vestibulum vestibulum. Aenean a mi non ex malesuada blandit id et massa. Nam vehicula massa vitae fringilla consectetur. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam consequat diam accumsan, placerat metus vel, gravida purus. Curabitur lobortis enim at dignissim sollicitudin. Aliquam augue quam, commodo a sem vel, venenatis sodales neque. Maecenas eu mollis sapien, vitae blandit quam. Quisque quis ultricies dolor. Praesent sodales urna nec ex accumsan gravida. Praesent quis tellus ut lectus mollis hendrerit. Maecenas finibus libero tellus. Ut semper posuere metus, at maximus orci facilisis in. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;         </p>
-        </BodyContainer>
-        <h2>Other interview reviews for this company</h2>
-        <OtherContainer>
-          {/* TODO: replace placeholders with db data */}
-          <ReviewPreviewCard
-            width="45%"
-            title="Frontend Engineer"
-            dateOfPost="20th March, 2020"
-            atCompany="Canva"
-            experience="Was great 10/10"
-            questionsAsked="Asked about experience and reversing a linked list"
-            offerReceived="Yes"
-            rating={5}
-          />
-          <ReviewPreviewCard
-            width="45%"
-            title="Frontend Engineer"
-            dateOfPost="20th March, 2020"
-            atCompany="Canva"
-            experience="Was great 10/10"
-            questionsAsked="Asked about experience and reversing a linked list"
-            offerReceived="Yes"
-            rating={5}
-          />
-        </OtherContainer>
-      </MainBodyContainer>
-      <Footer/>
+      <ErrorPopup
+        isOpen={errorMsg !== ""}
+        popupMessage={errorMsg}
+        handleClose={() => setErrorMsg("")}
+        type="error"
+      />
+      {isLoading
+        ? <LoadingOverlay isOpen={isLoading} />
+        : (
+          <>
+            <Header />
+            <MainBodyContainer>
+              <TopContainer>
+                {/* TODO: current placeholder till i figure out how to store images in db */}
+                <img src={CompanyIcon} alt="companyLogo" />
+                <TopCenterContainer>
+                  <h1>{`${jobTitle} (${companyName})`}</h1>
+                  <p>{dateOfPost}</p>
+                  <Rating value={rating} readOnly />
+                </TopCenterContainer>
+                <Tag
+                  backgroundColor={offerReceived ? Palette.jelpGreen : Palette.jelpRed}
+                >
+                  Offer received
+                </Tag>
+              </TopContainer>
+              <h2>Application/Interview Experience</h2>
+              <BodyContainer>
+                <p>
+                  {experience}
+                </p>
+              </BodyContainer>
+              <h2>Interview Questions</h2>
+              <BodyContainer>
+                <p>
+                  {questionsAsked}
+                </p>
+              </BodyContainer>
+              <h2>Other interview reviews for this company</h2>
+              <OtherContainer>
+                {/* TODO: replace placeholders with db data */}
+                <ReviewPreviewCard
+                  width="45%"
+                  title="Frontend Engineer"
+                  dateOfPost="20th March, 2020"
+                  atCompany="Canva"
+                  experience="Was great 10/10"
+                  questionsAsked="Asked about experience and reversing a linked list"
+                  offerReceived="Yes"
+                  rating={5}
+                />
+                <ReviewPreviewCard
+                  width="45%"
+                  title="Frontend Engineer"
+                  dateOfPost="20th March, 2020"
+                  atCompany="Canva"
+                  experience="Was great 10/10"
+                  questionsAsked="Asked about experience and reversing a linked list"
+                  offerReceived="Yes"
+                  rating={5}
+                />
+              </OtherContainer>
+            </MainBodyContainer>
+            <Footer/>
+          </>
+        )
+      }
     </InterviewReviewPageContainer>
   );
 };
