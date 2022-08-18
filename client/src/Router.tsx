72c7ea16f24edafca2b9b18ab96113182a250583
sunny_thinkpad <sunnyw987@gmail.com>
2022-08-18 19:14:21 +1000

    feat(frontend): implement protected routes

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Outlet, Navigate } from "react-router-dom";
 import CompanyListPage from "./pages/CompanyListPage/CompanyListPage";
 import CompanyReviewPage from "./pages/CompanyReviewPage/CompanyReviewPage";
 import HomePage from "./pages/HomePage/HomePage";
@@ -12,13 +12,12 @@ import RateCompanyPage from "./pages/RateCompanyPage/RateCompanyPage";
 import RateInterviewPage from "./pages/RateInterviewPage/RateInterviewPage";
 import SignUpPage from "./pages/SignUpPage/SignUpPage";
 
-// TODO: check that this works
-// const AuthenticatedRoute = (props: RouteProps) => {
-//   const token = sessionStorage.getItem(process.env.REACT_APP_TOKEN!);
-//   return (
-//     <Route {...props}>{!!token ? props.children : <LoginPage/>}</Route>
-//     );
-// };
+const AuthenticatedRoutes = () => {
+  const token = sessionStorage.getItem(process.env.REACT_APP_TOKEN!);
+  return (
+    !!token ? <Outlet /> : <Navigate to="/" />
+  );
+};
 
 const Router = () => (
     <BrowserRouter>
@@ -26,14 +25,16 @@ const Router = () => (
         <Route path='/' element={ <LandingPage /> } />
         <Route path='/signup' element={ <SignUpPage /> } />
         <Route path='/login' element={ <LoginPage /> } />
-        <Route path='/home' element={ <HomePage /> } />
-        <Route path='/rate/company' element={ <RateCompanyPage /> } />
-        <Route path='/rate/interview' element={ <RateInterviewPage /> } />
-        <Route path='/profile' element={ <ProfilePage /> } />
-        <Route path='/view/company/list' element={ <CompanyListPage /> } />
-        <Route path='/view/interview/list' element={ <InterviewListPage /> } />
-        <Route path='/view/company/:companyReviewId' element={ <CompanyReviewPage /> } />
-        <Route path='/view/interview/:interviewReviewId' element={ <InterviewReviewPage /> } />
+        <Route element={<AuthenticatedRoutes />}>
+          <Route path='/home' element={ <HomePage /> } />
+          <Route path='/rate/company' element={ <RateCompanyPage /> } />
+          <Route path='/rate/interview' element={ <RateInterviewPage /> } />
+          <Route path='/profile' element={ <ProfilePage /> } />
+          <Route path='/view/company/list' element={ <CompanyListPage /> } />
+          <Route path='/view/interview/list' element={ <InterviewListPage /> } />
+          <Route path='/view/company/:companyReviewId' element={ <CompanyReviewPage /> } />
+          <Route path='/view/interview/:interviewReviewId' element={ <InterviewReviewPage /> } />
+        </Route>
         <Route path="*" element={ <PageNotFound /> } />
       </Routes>
     </BrowserRouter>
