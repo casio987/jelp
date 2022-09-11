10ea58a1c2a6934a856555cee0bc92b71beb760a
sunny_thinkpad <sunnyw987@gmail.com>
2022-09-11 18:06:20 +1000

    feat(frontend): add loading overlay to sign up and login pages

--- client/src/pages/LoginPage/LoginPage.tsx
+++ client/src/pages/LoginPage/LoginPage.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { postLogin } from "../../api/auth";
+import LoadingOverlay from "../../components/LoadingOverlay/LoadingOverlay";
 import ErrorPopup from "../../components/Popup/Popup";
 import { InputField, LoginPageContainer, LogoText, SubText, LoginButton, RedirectLink, MainContainer } from "./style";
 
@@ -9,18 +10,21 @@ const LoginPage = () => {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [error, setError] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const loginUser = async () => {
     if (email === "" || password === "") {
       setError("All fields must be filled.");
       return;
     }
-
+    setIsLoading(true);
     try {
       const { data } = await postLogin(email, password);
       sessionStorage.setItem(process.env.REACT_APP_TOKEN!, data);
+      setIsLoading(false);
       navigate('/home');
     } catch (err: any) {
+      setIsLoading(false);
       setError(err.response.data || "A network error occurred. Please try again.")
     }
   }
@@ -33,25 +37,31 @@ const LoginPage = () => {
         handleClose={() => setError("")}
         type="error"
       />
-      <MainContainer>
-        <LogoText onClick={() => navigate('/')}>jelp</LogoText>
-        <SubText>
-          Sign in to&nbsp;
-          <span>
-            jelp
-          </span>
-        </SubText>
-        <InputField label="Email" error={error !== ""} onChange={(e) => setEmail(e.target.value)} />
-        <InputField label="Password" error={error !== ""} type="password" onChange={(e) => setPassword(e.target.value)} />
-        {/* TODO: include update password functionality */}
-        <LoginButton onClick={loginUser}>Login</LoginButton>
-        <span>
-          New to jelp?&nbsp;
-          <RedirectLink href="/signup">
-            Sign up
-          </RedirectLink>
-        </span>
-      </MainContainer>
+      {isLoading
+        ? (
+          <LoadingOverlay isOpen={isLoading} />
+        ) : (
+          <MainContainer>
+            <LogoText onClick={() => navigate('/')}>jelp</LogoText>
+            <SubText>
+              Sign in to&nbsp;
+              <span>
+                jelp
+              </span>
+            </SubText>
+            <InputField label="Email" error={error !== ""} onChange={(e) => setEmail(e.target.value)} />
+            <InputField label="Password" error={error !== ""} type="password" onChange={(e) => setPassword(e.target.value)} />
+            {/* TODO: include update password functionality */}
+            <LoginButton onClick={loginUser}>Login</LoginButton>
+            <span>
+              New to jelp?&nbsp;
+              <RedirectLink href="/signup">
+                Sign up
+              </RedirectLink>
+            </span>
+          </MainContainer>
+        )
+      }
     </LoginPageContainer>
   );
 };
--- client/src/pages/SignUpPage/SignUpPage.tsx
+++ client/src/pages/SignUpPage/SignUpPage.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { postRegister } from "../../api/auth";
+import LoadingOverlay from "../../components/LoadingOverlay/LoadingOverlay";
 import ErrorPopup from "../../components/Popup/Popup";
 import { SignUpPageContainer, LogoText, SubText, SubSubText, SignUpButton, RedirectLink, InputField, MainBody} from "./style";
 
@@ -9,6 +10,7 @@ const SignUpPage = () => {
   const [password, setPassword] = useState<string>("");
   const [confirmedPassword, setConfirmedPassword] = useState<string>("");
   const [error, setError] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const navigate = useNavigate();
 
@@ -21,11 +23,14 @@ const SignUpPage = () => {
       return;
     }
 
+    setIsLoading(true);
     try {
       const { data } = await postRegister(email, password);
       sessionStorage.setItem(process.env.REACT_APP_TOKEN!, data);
+      setIsLoading(false);
       navigate('/home');
     } catch (err: any) {
+      setIsLoading(false);
       setError(err.response.data || "A network error occurred. Please try again.");
     }
   }
@@ -38,23 +43,29 @@ const SignUpPage = () => {
         handleClose={() => setError("")}
         type="error"
       />
-      <MainBody>
-        <LogoText onClick={() => navigate('/')}>
-          jelp
-        </LogoText>
-        <SubText>Sign up now</SubText>
-        <SubSubText>Kick-start your job searching journey with us</SubSubText>
-        <InputField label="Email" error={error !== ""} onChange={(e) => setEmail(e.target.value)} />
-        <InputField label="Password" error={error !== ""} type="password" onChange={(e) => setPassword(e.target.value)} />
-        <InputField label="Confirm password" error={error !== ""} type="password" onChange={(e) => setConfirmedPassword(e.target.value)} />
-        <SignUpButton onClick={() => signUp()}>Sign Up</SignUpButton>
-        <span>
-          Already have an account? &nbsp;
-          <RedirectLink href="/login">
-            Sign in
-          </RedirectLink>
-        </span>
-      </MainBody>
+      {isLoading
+        ? (
+          <LoadingOverlay isOpen={isLoading} />
+        ) : (
+          <MainBody>
+            <LogoText onClick={() => navigate('/')}>
+              jelp
+            </LogoText>
+            <SubText>Sign up now</SubText>
+            <SubSubText>Kick-start your job searching journey with us</SubSubText>
+            <InputField label="Email" error={error !== ""} onChange={(e) => setEmail(e.target.value)} />
+            <InputField label="Password" error={error !== ""} type="password" onChange={(e) => setPassword(e.target.value)} />
+            <InputField label="Confirm password" error={error !== ""} type="password" onChange={(e) => setConfirmedPassword(e.target.value)} />
+            <SignUpButton onClick={() => signUp()}>Sign Up</SignUpButton>
+            <span>
+              Already have an account? &nbsp;
+              <RedirectLink href="/login">
+                Sign in
+              </RedirectLink>
+            </span>
+          </MainBody>
+        )
+      }
     </SignUpPageContainer>    
   );
 };
